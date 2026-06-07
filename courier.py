a52d5397766eba5728af0750bdceb942b99e40a2
faxridddinoz <bebebebebdededede9@gmail.com>
2026-06-07 17:00:12 +0500

    Update courier.py

--- courier.py
+++ courier.py
@@ -2,10 +2,10 @@
 # SOZLAMALAR - BU YERNI TO'LDIRING
 # ========================
 
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = "8749714916:AAFY3Vf_1NoFOSR5EhutHBNsq_Z0p-XH-GI"
 
 # Birinchi admin ID (Telegram user ID)
-SUPER_ADMIN_ID = 123456789  # O'zingizning Telegram ID ingizni kiriting
+SUPER_ADMIN_ID = 8684039353  # O'zingizning Telegram ID ingizni kiriting
 
 # Ma'lumotlar bazasi fayli
 DB_FILE = "food_bot.db"
